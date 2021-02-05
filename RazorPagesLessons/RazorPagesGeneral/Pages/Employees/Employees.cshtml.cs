7c024f599a279818497b246d18f1711a68cb3e0b
Maxim-80 <18kmv@mail.ru>
2021-02-05 23:18:43 +0300

    добавил проект для работы с бд

--- RazorPagesLessons/RazorPagesGeneral/Pages/Employees/Employees.cshtml.cs
+++ RazorPagesLessons/RazorPagesGeneral/Pages/Employees/Employees.cshtml.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesLessons.Models;
+using RazorPagesLessons.Services;
 
 namespace RazorPagesGeneral.Pages.Employees
 {
     public class EmployeesModel : PageModel
     {
+        private readonly IEmployeeRepository _db;
+
+        public IEnumerable<Employee> Employees { get; set; }
+
+        public EmployeesModel(IEmployeeRepository db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
-
+            Employees = _db.GetAllEmployees();
         }
     }
 }
---
+++ RazorPagesLessons/RazorPagesLessons.Services/MockEmployeeRepository.cs
@@ -0,0 +1,47 @@
+﻿using RazorPagesLessons.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazorPagesLessons.Services
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 0,
+                    Name = "Mary",
+                    Email = "mary@expl.com",
+                    PhotoPath = "avatar2.png",
+                    Department = Dept.HR
+                },
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Tom",
+                    Email = "tom@expl.com",
+                    PhotoPath = "avatar.png",
+                    Department = Dept.IT
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Bob",
+                    Email = "bob@expl.com",
+                    Department = Dept.Payroll
+                }
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+    }
+}
