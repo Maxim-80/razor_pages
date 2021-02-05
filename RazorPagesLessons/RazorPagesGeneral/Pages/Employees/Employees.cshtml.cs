5ff66c39e6d52c042ea15928d2a120261a93f5e3
Maxim_80 <18kmv@mail.ru>
2021-02-05 18:00:56 +0300

    склонировал на рабочий пк репозиторий добавил страницу Employees

---
+++ RazorPagesLessons/RazorPagesGeneral/Pages/Employees/Employees.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesGeneral.Pages.Employees
+{
+    public class EmployeesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
